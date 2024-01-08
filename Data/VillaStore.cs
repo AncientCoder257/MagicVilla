7d736f2da49db70f68468e601d88d45157faa5c0
Abdul Charfaray <abdulc@runninghill.co.za>
2024-01-08 09:49:11 +0200

    Added more fields to dto and updated the villaStore with new fields.

--- Data/VillaStore.cs
+++ Data/VillaStore.cs
@@ -9,12 +9,16 @@ public class VillaStore
         new VillaDTO
         {
             Id = 1,
-            Name = "Pool View"
+            Name = "Pool View",
+            Sqm = 100,
+            Occupancy = 4
         },
         new VillaDTO
         {
             Id = 2,
-            Name = "Another View"
+            Name = "Another View",
+            Sqm = 200,
+            Occupancy = 6
         }
     };
 }
