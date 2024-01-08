017a0306afe88b3a44bff98cbeef2c533392789d
Abdul Charfaray <abdulc@runninghill.co.za>
2024-01-08 09:48:05 +0200

    added custom error to createVilla. Added Delete, Put and Patch to villaApi.

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -1,5 +1,6 @@
 using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers;
@@ -37,8 +38,15 @@ public class VillaAPIController : ControllerBase
     [ProducesResponseType(typeof(VillaDTO), StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+    public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO? villaDTO)
     {
+        var villaName = VillaStore.villaList
+            .FirstOrDefault(v => v.Name?.ToLower() == villaDTO.Name.ToLower());
+        if (villaName != null)
+        {
+            ModelState.AddModelError("", "Villa already Exists!");
+            return BadRequest(ModelState);
+        }
         if (villaDTO == null)
         {
             return BadRequest();
@@ -52,8 +60,82 @@ public class VillaAPIController : ControllerBase
         villaDTO.Id = VillaStore.villaList.Any()
             ? VillaStore.villaList.MaxBy(v => v.Id)!.Id + 1 
             : 1;
+        /* this is the given code in the course
+         villaDTO.Id = VillaStore.villaList
+            .OrderByDescending(v => v.Id)
+            .FirstOrDefault().Id + 1;
+            */
+        
         VillaStore.villaList.Add(villaDTO);
 
         return CreatedAtRoute("GetVilla", new {id = villaDTO.Id},villaDTO);
+    }
+
+    [HttpDelete("{id:int}", Name = "DeleteVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult DeleteVilla(int id)
+    {
+        if (id == 0)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList
+            .FirstOrDefault(v => v.Id == id);
+        if (villa == null)
+        {
+            return NotFound();
+        }
+
+        VillaStore.villaList.Remove(villa);
+        return NoContent();
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult UpdateVilla(int id, [FromBody] VillaDTO? villaDto)
+    {
+        if (villaDto == null || id != villaDto.Id)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList
+            .FirstOrDefault(v => v.Id == id);
+        villa.Name = villaDto.Name;
+        villa.Sqm = villaDto.Sqm;
+        villa.Occupancy = villaDto.Occupancy;
+
+        return NoContent();
+    }
+
+    [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDTO>? patchDto)
+    {
+        //https://jsonpatch.com/ 
+        //https://learn.microsoft.com/en-us/aspnet/core/web-api/jsonpatch?view=aspnetcore-8.0
+        if (patchDto == null || id == 0)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList
+            .FirstOrDefault(v => v.Id == id);
+        if(villa == null)
+        {
+            return BadRequest();
+        }
+        patchDto.ApplyTo(villa, ModelState);
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        return NoContent();
     }
 }
