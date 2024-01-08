cbe486cd4475c6d997618de0fcddfe302399c655
Abdul Charfaray <abdulc@runninghill.co.za>
2024-01-08 09:49:11 +0200

    Added more fields to dto and updated the villaStore with new fields.

--- Models/Dto/VillaDTO.cs
+++ Models/Dto/VillaDTO.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MagicVilla_VillaAPI.Models.Dto;
 
 public class VillaDTO
 {
     public int Id { get; set; }
+    [Required]
     public string? Name { get; set; }
+
+    public int? Occupancy { get; set; }
+    public int? Sqm { get; set; }
 }
