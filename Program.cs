08fbe6c68edc57b345c1058989952dc9e6fbafda
Abdul Charfaray <abdulc@runninghill.co.za>
2024-01-08 20:59:48 +0200

    Added Serilog and dependency injection.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel
+    .Debug()
+    .WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Add services to the container.
